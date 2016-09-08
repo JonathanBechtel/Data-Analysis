8ed12212f3cea429126ce99949174494c0a93cb0
JonathanBechtel <JonathanBechtel@gmail.com>
2016-09-08 19:45:16 -0400

    Made modifications to find_C

--- kaggle/titanic.py
+++ kaggle/titanic.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
+#Read files into the program
 test = pd.read_csv("C:\Users\Ohio\Documents\Kaggle\Titanic Survivors\_test.csv")
 train = pd.read_csv("C:\Users\Ohio\Documents\Kaggle\Titanic Survivors\_train.csv")
 #train = pd.concat([train, test])
 
+#Drop unnecessary columns
 train = train.drop(train.columns[[0, 8]], axis=1)
+
+#Encode categorical data to numbers for further modification
 train['Sex'] = LabelEncoder().fit_transform(train.Sex)
 train['Cabin'] = LabelEncoder().fit_transform(pd.notnull(train.Cabin))
 train.Embarked = train.Embarked.fillna(value='S')
@@ -17,6 +21,7 @@ train['Embarked'] = LabelEncoder().fit_transform(train.Embarked)
 train['Family_Size'] = train.SibSp + train.Parch
 train['Age'] = train.groupby(['Pclass', 'Sex'])['Age'].transform(lambda x: x.replace(np.nan, x.median())) 
 
+#Used to create new pd Series from Name data that extracts the greeting used for their name to be used as a separate variable
 def find_greeting(value):
     greetings = ['Mrs.', 'Miss.', 'Mr.', 'Master.', 'Rev.', 'Don.', 'Dr.', 'Mme.', 'Ms.', 'Major.', 'Lady.', 'Sir.', 'Mlle.', 'Col.', 'Capt.', 'Countess.']
     words = value.split()
@@ -31,6 +36,7 @@ train['Greeting'] = train.Greeting.fillna('Mr.')
 train['Greeting'] = LabelEncoder().fit_transform(train.Greeting)
 del train['Name']
 
+#Reorder columns to group continuous, categorical data together
 train = train[['Pclass', 'Sex', 'Cabin', 'Embarked', 'Greeting', 'SibSp', 'Parch', 'Family_Size', 'Age', 'Fare', 'Survived']]
 
 #Categorical coding for data with more than two labels
@@ -45,14 +51,19 @@ train['Family_scaled'] = (train.Family_Size - train.Family_Size.mean())/train.Fa
 train['Age_scaled'] = (train.Age - train.Age.mean())/train.Age.std()
 train['Fare_scaled'] = (train.Fare - train.Fare.mean())/train.Fare.std()
 
+#Drop unmodified data since it's no longer needed
 X = train.drop(train.columns[[0,3,4,5,6,7,8,9,10]], axis=1)
+
+#Concat modified data to be used for analysis, set to X and y values
 X = pd.concat([X, Greetings, Pclass, Embarked], axis=1)
 y = train.Survived
 
+#Create cross - validation set 
 X_train, X_val, y_train, y_val = train_test_split(X, y, train_size = 0.6, random_state=0)
 
 clf = LogisticRegression(solver='lbfgs')
  
+#Function used to determine optimal lambda value for regularization
 def find_C(X, y):
     Cs = np.logspace(-4, 4, 10)
     error = []  
@@ -68,5 +79,6 @@ def find_C(X, y):
     plt.title('What\'s the Best Value of C?')
     clf.C = Cs[error.index(max(error))]
     print("Ideal value of C is %d" % (Cs[error.index(max(error))]))
-    
+ 
+#Sets logistic regression to the value of C that corresponds to the highest score on the CV set  
 find_C(X_val, y_val)
