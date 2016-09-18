836fc5aa6ed30fff3a9ed9e49c3ff873247679ae
JonathanBechtel <JonathanBechtel@gmail.com>
2016-09-18 19:16:53 -0400

    Made small adjustments

--- kaggle/titanic_alt.py
+++ kaggle/titanic_alt.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn import svm
 
 #Read files into the program
 test = pd.read_csv("C:\Users\Ohio\Documents\Kaggle\Titanic Survivors\_test.csv", index_col='PassengerId')
 train = pd.read_csv("C:\Users\Ohio\Documents\Kaggle\Titanic Survivors\_train.csv", index_col='PassengerId')
-"""
 y = train['Survived']
 del train['Survived']
 
@@ -19,14 +18,12 @@ train = pd.concat([train, test])
 #Drop unnecessary columns
 train = train.drop(train.columns[[6,8,9]], axis=1)
 
+
 #Encode categorical data to numbers for further modification
 train['Sex'] = LabelEncoder().fit_transform(train.Sex)
-train['Pclass'] = LabelEncoder().fit_transform(train.Pclass)
-
+train['Family_Size'] = train.SibSp + train.Parch
 train['Age'] = train.groupby(['Pclass', 'Sex'])['Age'].transform(lambda x: x.replace(np.nan, x.median())) 
 train.iloc[1043, 6] = 7.90
-
-train['Family_Size'] = train.SibSp + train.Parch
 
 #Used to create new pd Series from Name data that extracts the greeting used for their name to be used as a separate variable
 def greeting_search(words):
@@ -39,12 +36,14 @@ train['Greeting'] = train.groupby('Greeting')['Greeting'].transform(lambda x: 'R
 train['Greeting'] = LabelEncoder().fit_transform(train.Greeting)
 del train['Name']
 
+
 #Reorder columns to group continuous, categorical data together
 train = train[['Pclass', 'Sex', 'Greeting', 'SibSp', 'Parch', 'Family_Size', 'Age', 'Fare']]
 
 #Categorical coding for data with more than two labels
-Pclass = pd.get_dummies(train['Pclass'], prefix='Passenger Class', drop_first=True)
 Greetings = pd.get_dummies(train['Greeting'], prefix='Greeting', drop_first=True)
+Pclass = pd.get_dummies(train['Pclass'], prefix='Passenger Class', drop_first=True)
+#Cabins = pd.get_dummies(train['Cabin'], prefix='Cabin', drop_first=True)
 
 #Scale Continuous Data
 train['SibSp_scaled'] = (train.SibSp - train.SibSp.mean())/train.SibSp.std()
@@ -59,14 +58,13 @@ train = train.drop(train.columns[[0,2,3,4,5,6,7]], axis=1)
 #Concat modified data to be used for analysis, set to X and y values
 data = pd.concat([train, Greetings, Pclass], axis=1)
 
-#Split the data back into its original training and test sets
 test = data.iloc[891:]
 X = data[:891]
 
 #Create cross - validation set 
 X_train, X_val, y_train, y_val = train_test_split(X, y, train_size = 0.6)
 
-clf = LogisticRegression()
+clf = LogisticRegression(solver='lbfgs')
  
 #Function used to determine optimal lambda value for regularization
 def find_C(X, y):
@@ -84,7 +82,7 @@ def find_C(X, y):
     plt.title('What\'s the Best Value of C?')
     plt.show()
     clf.C = Cs[score.index(max(score))]
-    print("Ideal value of C is %g" % (Cs[score.index(max(score))]))
+    print("Ideal value of C is %d" % (Cs[score.index(max(score))]))
     print(max(score))
  
  
@@ -93,8 +91,8 @@ find_C(X_val, y_val)
 clf.fit(X, y)
 
 
-answer = pd.DataFrame(clf.predict(test), index=test.index, columns=['Survived'])
-answer.to_csv('C:\Users\Ohio\Documents\Kaggle\Titanic Survivors\_answer.csv')
+alt_answer = pd.DataFrame(clf.predict(test), index=test.index, columns=['Survived'])
+alt_answer.to_csv('C:\Users\Ohio\Documents\Kaggle\Titanic Survivors\_alt_answer.csv')
 coef = pd.DataFrame({'Variable': data.columns, 'Coefficient': clf.coef_[0]})
 
 results = y_val.tolist()
@@ -102,7 +100,7 @@ predict = clf.predict(X_val)
 
 def precision_recall(predictions, results):
     
-    tp, fp, fn, tn, i = 0.0, 0.0, 0.0, 0.0, 0
+    tp, fp, fn, tn, i = 0, 0, 0, 0, 0
     
     while i < len(results):
         
@@ -116,10 +114,7 @@ def precision_recall(predictions, results):
                 fn = fn + 1
             i = i + 1
     
-    precision = tp / (tp + fp)
-    recall = tn / (tn + fn)
-    f1 = 2*precision*recall / (precision + recall)
-    print("Precision: %g, Recall: %g, f1: %g" % (precision, recall, f1))
-    
-precision_recall(predict, results)
-"""
+    precision = float(4)/5.0
+    recall = tp + fp
+    accuracy = precision/recall
+    print("Precision: %d, Recall: %d, accuracy: %d" % (precision, recall, accuracy))
