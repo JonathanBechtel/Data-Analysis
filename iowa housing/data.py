6da284cd86f16220a84bf5492c2955341787173d
JonathanBechtel <JonathanBechtel@gmail.com>
2016-09-21 13:21:26 -0400

    Added additional data munging

--- iowa housing/data.py
+++ iowa housing/data.py
@@ -1,12 +1,21 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing as pp
+from sklearn.linear_model import LogisticRegression
 import scipy.stats as stats
+from sklearn.cross_validation import train_test_split
 
-train = pd.read_csv("C:\Users\Ohio\Documents\GitHub\data-analysis\iowa housing\_train.csv")
-test = pd.read_csv("C:\Users\Ohio\Documents\GitHub\data-analysis\iowa housing\_test.csv")
+train = pd.read_csv("C:\Users\Ohio\Documents\GitHub\data-analysis\iowa housing\_train.csv", index_col='Id')
+test = pd.read_csv("C:\Users\Ohio\Documents\GitHub\data-analysis\iowa housing\_test.csv", index_col='Id')
 
 train['SalePrice'] = pp.scale(np.log(train.SalePrice))
 y = train.SalePrice
 del train['SalePrice']
 data = pd.concat([train, test])
+
+data['MSSubClass'] = data.MSSubClass.apply(lambda x: str(x))
+numerical_feats = data.dtypes[data.dtypes != 'object'].index
+data[numerical_feats] = data[numerical_feats].fillna(data[numerical_feats].median())
+data[numerical_feats] = data[numerical_feats].apply(lambda x: x.where(x == 0, np.log(x)))
+data[numerical_feats] = pp.scale(data[numerical_feats])
+data = pd.get_dummies(data, drop_first=True)
